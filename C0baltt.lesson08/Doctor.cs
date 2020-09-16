2e738a0f65607c3495c8622f4d1ce87f0c7a0c0b
Александр Жаворонок <C0baltt@live.com>
2020-09-16 16:08:30 +0300

    Добавьте файлы проекта.

---
+++ C0baltt.lesson08/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace C0baltt.lesson08
+{
+    class Doctor : Person
+    {
+        void m()
+        {
+           public string Specialization
+        }
+    }
+}
---
+++ C0baltt.lesson08/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0baltt.lesson08
+{
+    public class Person
+    {
+        public string Firstname;
+        public string Lastname;
+
+        protected decimal Money;
+    }
+}
+/*class Person
+    {
+        public //открытый
+        internal //открытый в пределах проекта
+        protected//защищенный
+        private //закрытый
+    }*/
