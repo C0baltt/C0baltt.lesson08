75c483b9e05ef1fe31624503242160f885dc03d4
Александр Жаворонок <C0baltt@live.com>
2020-09-22 14:27:12 +0300

    7

--- C0baltt.lesson08/Person.cs
+++ C0baltt.lesson08/Person.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 
 namespace C0baltt.lesson08
 {
+    public class Person//создаем открытый класс Person (человек)
+    {
+        public string Firstname;//добавляем открытое значение Firstname (имя)
+        public string Lastname;//добавляем открытое значение Lastname (фамилия)
+
+        protected decimal Money;//добавляем защищенное значение Money (деньги)
+    }
+}
+/*                          Конспект
+class Person
+    {
+        public //открытый
+        internal //открытый в пределах проекта
+        protected//защищенный(будет доступно у наследника)
+        private //закрытый(не будет доступно у наследника, т. е. есть только у данного класса)
+    }
+namespace C0baltt.lesson08
+{
     public class Person//создаем открытый класс Person (человк)
     {
         public string Firstname;//добавляем открытое значение Firstname (имя)
@@ -15,10 +33,4 @@ namespace C0baltt.lesson08
         protected decimal Money;//добавляем защищенное значение Money (деньги)
     }
 }
-/*class Person
-    {
-        public //открытый
-        internal //открытый в пределах проекта
-        protected//защищенный(будет доступно у наследника)
-        private //закрытый(не будет доступно у наследника, т. е. есть только у данного класса)
-    }*/
+*/
--- C0baltt.lesson08/Program.cs
+++ C0baltt.lesson08/Program.cs
@@ -6,28 +6,39 @@ using System.Threading.Tasks;
 
 namespace C0baltt.lesson08
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Person person = new Person();//создаем объект person класса Person
-            person.Firstname = "Вадим";//записываем в свойство Firstname (имя) значение "Вадим"
-            person.Lastname = "Шванов";//записываем в свойство Lastname (фамилия) значение "Шванов"
-
-            Person p2 = new Person();//создаем объект p2 класса Person
-            p2.Firstname = "Иван";//записываем в свойство Firstname (имя) значение "Иван"
-            p2.Lastname = "Иванов";//записываем в свойство Lastname (фамилия) значение "Иванов"
-
-            var d = new Doctor();//создаем объект d класса Doctor
-            d.Firstname = "Роман";//записываем в свойство Firstname (имя) значение "Роман"
-            d.Specialization = "333";//записываем в свойство 333 (специализация) значение "Роман"
-
-            Console.WriteLine(d.Firstname);
-            Console.WriteLine(d.Specialization);
-
-            Person p = d;
-
-            Console.WriteLine(p);
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
 }
+/*                          Конспект
+ namespace C0baltt.lesson08
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person person = new Person();//создаем объект person класса Person
+			person.Firstname = "Вадим";//записываем в свойство Firstname (имя) значение "Вадим"
+			person.Lastname = "Шванов";//записываем в свойство Lastname (фамилия) значение "Шванов"
+
+			Person p2 = new Person();//создаем объект p2 класса Person
+			p2.Firstname = "Иван";//записываем в свойство Firstname (имя) значение "Иван"
+			p2.Lastname = "Иванов";//записываем в свойство Lastname (фамилия) значение "Иванов"
+
+			var d = new Doctor();//создаем объект d класса Doctor
+			d.Firstname = "Роман";//записываем в свойство Firstname (имя) значение "Роман"
+			d.Specialization = "333";//записываем в свойство 333 (специализация) значение "Роман"
+
+			Console.WriteLine(d.Firstname);
+			Console.WriteLine(d.Specialization);
+
+			Person p = d;
+
+			Console.WriteLine(p);
+		}
+	}
+}*/
